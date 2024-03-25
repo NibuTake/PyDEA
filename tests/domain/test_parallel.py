ab630887d07333aca5229627408ffc71adc39406
NibuTake <0815takeshi@gmail.com>
2024-03-25 21:47:12 +0900

    💚 added support for GitHub actions

--- tests/domain/test_parallel.py
+++ tests/domain/test_parallel.py
@@ -5,7 +5,7 @@ from Pyfrontier.domain.parallel import MultiProcessor, NumberOfJobs
 class TestNumberOfJobs:
     @pytest.mark.parametrize(
         "n_jobs",
-        [1, 5],
+        [1, 2],
     )
     def test_happy_path(self, n_jobs):
         assert NumberOfJobs(n_jobs).value == n_jobs
